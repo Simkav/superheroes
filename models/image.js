916f0c3d54e45767b4905c695c22717cd41c833a
Ivan Khomenko <Simkav2411@gmail.com>
2021-04-09 17:03:26 +0300

    Add image model and migration

---
+++ models/image.js
@@ -0,0 +1,28 @@
+'use strict';
+const { Model } = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Image extends Model {
+    static associate (models) {
+      Image.belongsTo(models.SuperHero, { foreignKey: 'superHeroId' });
+    }
+  }
+  Image.init(
+    {
+      path: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          notNull: true,
+          notEmpty: true,
+        },
+      },
+    },
+    {
+      sequelize,
+      modelName: 'Image',
+      tableName: 'images',
+      underscored: true,
+    }
+  );
+  return Image;
+};
