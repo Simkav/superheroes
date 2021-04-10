4f682bf3f82e60cd36d35bf0359df7e6daccd426
Ivan Khomenko <Simkav2411@gmail.com>
2021-04-10 10:08:09 +0300

    Add error handler in app

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const router = require('./routes');
 const app = express();
+const errorHandler = require('./middlewares/errorHandler.mw');
 
 app.use(express.json());
 
 app.use('/api', router);
+
+app.use(errorHandler);
 
 module.exports = app;
