82f7f13c7ee2d0000811d20cf8dc92ed4def39e0
Ivan Khomenko <Simkav2411@gmail.com>
2021-04-12 12:09:37 +0300

    Add multer mw for upload files

---
+++ middlewares/multer.mw.js
@@ -0,0 +1,13 @@
+const multer = require('multer');
+const { STATIC_IMAGES_PATH } = require('../config');
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, STATIC_IMAGES_PATH);
+  },
+  filename: (req, file, cb) => {
+    cb(null, `${Date.now()}.${file.originalname}`);
+  },
+});
+
+module.exports = multer({ storage });
