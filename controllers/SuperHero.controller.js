569ae0e01afe2448ef7d589ac364d899f92d9231
Ivan Khomenko <Simkav2411@gmail.com>
2021-04-12 12:09:27 +0300

    Add CRUD methods to superhero

--- controllers/SuperHero.controller.js
+++ controllers/SuperHero.controller.js
@@ -1,16 +1,30 @@
 const createError = require('http-errors');
-const { SuperHero } = require('../models');
+const { SuperHero, SuperPower, Image } = require('../models');
+const pickBody = require('../utils/pickBody');
+const validatePage = require('../utils/validatePage');
 
-module.exports.getSuperHeroes = async (req, res, next) => {
+const avalibleValues = [
+  'nickname',
+  'realName',
+  'originDescription',
+  'catchPhrase',
+];
+
+module.exports.getHeroes = async (req, res, next) => {
   try {
-    const heroes = await SuperHero.findAll();
-    res.status(200).send(heroes);
+    const {
+      query: { page },
+    } = req;
+    const offset = await validatePage(page);
+    const heroes = await SuperHero.findAll({ limit: 5, offset });
+    res.status(200).send({ data: { heroes } });
   } catch (err) {
     console.log(err);
     next(err);
   }
 };
-module.exports.createSuperHero = async (req, res, next) => {
+
+module.exports.createHero = async (req, res, next) => {
   try {
     const { body } = req;
 
@@ -18,36 +32,252 @@ module.exports.createSuperHero = async (req, res, next) => {
     if (!createdHero) {
       return next(createError(400));
     }
-    res.status(201).send(createdHero);
+    res.status(201).send({ data: { createdHero } });
   } catch (err) {
     console.log(err);
     next(err);
   }
 };
-module.exports.getSuperHeroById = async (req, res, next) => {
+
+module.exports.getHeroById = async (req, res, next) => {
   try {
     const { heroInstance } = req;
-    res.status(200).send(heroInstance);
+    res.status(200).send({ data: { heroInstance } });
   } catch (err) {
     console.log(err);
     next(err);
   }
 };
+
 module.exports.deleteHero = async (req, res, next) => {
   try {
     const { heroInstance } = req;
     await hero.destroy();
-    res.status(200).send(heroInstance);
+    res.status(200).send({ data: { heroInstance } });
   } catch (err) {
     console.log(err);
     next(err);
   }
 };
+
 module.exports.updateHero = async (req, res, next) => {
   try {
-    const { parsedBody, heroInstance } = req;
+    const { body, heroInstance } = req;
+    const parsedBody = pickBody(body, avalibleValues);
     await heroInstance.update(parsedBody);
-    res.status(200).send(heroInstance);
+    res.status(200).send({ data: { heroInstance } });
+  } catch (err) {
+    console.log(err);
+    next(err);
+  }
+};
+
+module.exports.createHeroWithImages = async (req, res, next) => {
+  try {
+    const { body, files } = req;
+
+    const hero = await SuperHero.create(body);
+    if (!hero) {
+      return next(createError(404, 'Hero not created'));
+    }
+
+    const imageNames = files.map(image => {
+      return { path: image.filename, heroId: hero.dataValues.id };
+    });
+
+    const createdImages = await Image.bulkCreate(imageNames);
+
+    if (!createdImages) {
+      return next(createError(404, 'Error while creating image'));
+    }
+
+    res.send({ data: { hero, createdImages } });
+  } catch (err) {
+    console.log(err);
+    next(err);
+  }
+};
+module.exports.createHeroWithPowers = async (req, res, next) => {
+  try {
+    const { body } = req;
+
+    const hero = await SuperHero.create(body);
+    if (!hero) {
+      return next(createError(404, 'Hero not created'));
+    }
+
+    const powers = body.superpowers.map(power => {
+      return {
+        power,
+      };
+    });
+
+    const createdPowers = await SuperPower.bulkCreate(powers);
+
+    if (!createdPowers) {
+      return next(createError(404, 'Error while creating power'));
+    }
+
+    await hero.addSuperPowers(createdPowers);
+
+    res.send({ data: { hero, createdPowers } });
+  } catch (err) {
+    console.log(err);
+    next(err);
+  }
+};
+
+module.exports.createFullHero = async (req, res, next) => {
+  try {
+    const { body, files } = req;
+
+    const hero = await SuperHero.create(body);
+    if (!hero) {
+      return next(createError(404, 'Hero not created'));
+    }
+
+    const imagePaths = files.map(image => {
+      return { path: image.filename, heroId: hero.dataValues.id };
+    });
+
+    const createdImages = await Image.bulkCreate(imagePaths);
+
+    if (!createdImages) {
+      return next(createError(404, 'Error while creating image'));
+    }
+
+    const powers = body.superpowers.map(power => {
+      return {
+        power,
+      };
+    });
+
+    const createdPowers = await SuperPower.bulkCreate(powers);
+
+    if (!createdPowers) {
+      return next(createError(404, 'Error while creating power'));
+    }
+    await hero.addSuperPowers(createdPowers);
+
+    res.send({ data: { hero, createdImages, createdPowers } });
+  } catch (err) {
+    console.log(err);
+    next(err);
+  }
+};
+
+module.exports.getImagesFromHero = async (req, res, next) => {
+  try {
+    const { heroInstance } = req;
+
+    const heroImages = await heroInstance.getImages();
+
+    res.status(200).send({ data: { heroImages } });
+  } catch (er) {
+    console.log(err);
+    next(err);
+  }
+};
+
+module.exports.addImages = async (req, res, next) => {
+  try {
+    const { files, heroInstance } = req;
+
+    const heroId = heroInstance.id;
+
+    const imagePaths = files.map(image => {
+      return { path: image.filename, heroId };
+    });
+
+    const addedImages = await Image.bulkCreate(imagePaths);
+
+    res.status(200).send({ data: { addedImages } });
+  } catch (err) {
+    console.log(err);
+    next(err);
+  }
+};
+
+module.exports.removeImageFromHero = async (req, res, next) => {
+  try {
+    const {
+      params: { imageId },
+      heroInstance,
+    } = req;
+
+    const heroId = heroInstance.id;
+
+    const heroImage = await Image.findOne({
+      where: { heroId, id: imageId },
+      returning: true,
+    });
+
+    if (!heroImage) {
+      return next(createError(404, 'Cant find image'));
+    }
+
+    const returnedImages = heroImage;
+
+    await heroImage.destroy();
+
+    res.status(200).send({ data: { returnedImages } });
+  } catch (err) {
+    console.log(err);
+    next(err);
+  }
+};
+
+module.exports.getSuperPowersFromHero = async (req, res, next) => {
+  try {
+    const { heroInstance } = req;
+
+    const heroPowers = await heroInstance.getSuperPowers();
+
+    res.status(200).send({ data: { heroPowers } });
+  } catch (err) {
+    console.log(err);
+    next(err);
+  }
+};
+
+module.exports.addSuperPowersToHero = async (req, res, next) => {
+  try {
+    const { body, heroInstance } = req;
+
+    const powers = body.superpowers.map(power => {
+      return {
+        power,
+      };
+    });
+
+    const createdPowers = await SuperPower.bulkCreate(powers);
+
+    if (!createdPowers) {
+      return next(createError(404, 'Error while creating power'));
+    }
+    await heroInstance.addSuperPowers(createdPowers);
+
+    res.send({ data: { createdPowers } });
+  } catch (err) {
+    console.log(err);
+    next(err);
+  }
+};
+
+module.exports.removeSuperPowerFromHero = async (req, res, next) => {
+  try {
+    const {
+      heroInstance,
+      params: { powerId },
+    } = req;
+
+    const powerInstance = await SuperPower.findByPk(powerId);
+
+    const removedHeroPowers = await heroInstance.removeSuperPowers(
+      powerInstance
+    );
+
+    res.statsu(200).send({ data: { removedHeroPowers } });
   } catch (err) {
     console.log(err);
     next(err);
--- routes/superHero.js
+++ routes/superHero.js
@@ -1,21 +1,66 @@
 const { Router } = require('express');
 const SuperHeroController = require('../controllers/SuperHero.controller');
 const heroInstance = require('../middlewares/heroInstance.mw');
-const pickBody = require('../middlewares/pickBody.mw');
+const upload = require('../middlewares/multer.mw');
 
 const superHeroRouter = Router();
 
-superHeroRouter.get('/', SuperHeroController.getSuperHeroes);
-superHeroRouter.get('/:id', heroInstance, SuperHeroController.getSuperHeroById);
+superHeroRouter.get('/', SuperHeroController.getHeroes);
+superHeroRouter.get('/:id', heroInstance, SuperHeroController.getHeroById);
+superHeroRouter.get(
+  '/:id/getImages',
+  heroInstance,
+  SuperHeroController.getImagesFromHero
+);
+superHeroRouter.get(
+  '/:id/getPowers',
+  heroInstance,
+  SuperHeroController.getSuperPowersFromHero
+);
 
-superHeroRouter.post('/', SuperHeroController.createSuperHero);
+superHeroRouter.post('/', SuperHeroController.createHero);
 
 superHeroRouter.delete('/:id', heroInstance, SuperHeroController.deleteHero);
-superHeroRouter.patch(
-  '/:id',
+superHeroRouter.delete(
+  '/:id/deleteImage/:imageId',
   heroInstance,
-  pickBody,
-  SuperHeroController.updateHero
+  SuperHeroController.removeImageFromHero
+);
+superHeroRouter.delete(
+  '/:id/deletePower/:powerId',
+  heroInstance,
+  SuperHeroController.removeSuperPowerFromHero
+);
+
+superHeroRouter.patch('/:id', heroInstance, SuperHeroController.updateHero);
+
+superHeroRouter.post(
+  '/withImages',
+  upload.array('images', 10),
+  SuperHeroController.createHeroWithImages
+);
+superHeroRouter.post(
+  '/withPowers',
+  heroInstance,
+  SuperHeroController.createHeroWithPowers
+);
+superHeroRouter.post(
+  '/fullHero',
+  upload.array('images', 10),
+  SuperHeroController.createFullHero
+);
+
+superHeroRouter.patch(
+  '/:id/addPowers',
+  heroInstance,
+  SuperHeroController.addSuperPowersToHero
+);
+
+superHeroRouter.patch(
+  '/:id/addImages',
+  heroInstance,
+  upload.array('images', 10),
+  SuperHeroController.addImages
 );
 
 module.exports = superHeroRouter;
