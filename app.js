b07c600353da47c288ef9e2038a2b417589e2113
Ivan Khomenko <Simkav2411@gmail.com>
2021-04-13 13:18:12 +0300

    Add expres static for requests images

--- app.js
+++ app.js
@@ -3,10 +3,13 @@ const router = require('./routes');
 const app = express();
 const errorHandler = require('./middlewares/errorHandler.mw');
 const createPublicFolder = require('./utils/createPublicFolder');
+const { STATIC_IMAGES_PATH } = require('./config/index');
 
 createPublicFolder();
 
 app.use(express.json());
+
+app.use(express.static(STATIC_IMAGES_PATH));
 
 app.use('/api', router);
 
